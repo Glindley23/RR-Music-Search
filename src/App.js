a443276f84f886a622d08ca0c1ffb2a43ef8a894
Glindley23 <graham.lindley23@gmail.com>
2023-01-22 14:03:54 -0500

    finished assignment

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
-import {useEffect, useState } from 'react'
+import {useEffect, useState, Fragment } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Gallery from './components/Gallery'
 import SearchBar from './components/SearchBar'
+import AlbumView from './components/AlbumView'
+import ArtistView from './components/ArtistView'
 import { DataContext } from './context/DataContext'
 
 function App() {
@@ -32,14 +35,26 @@ function App() {
 	}
 
 	return (
-		<div>
-			<SearchBar handleSearch = {handleSearch}/>
-			{message}
-			<DataContext.Provider value={data}>
-				<Gallery />
-			</DataContext.Provider>
-		</div>
-  	);
+		<DataContext.Provider value={data}>
+			<div>
+				{message}
+				<Router>
+					<Routes>
+						<Route path="/" element={
+							<Fragment>
+								<SearchBar handleSearch={handleSearch} />
+								<Gallery data={data} />
+							</Fragment>
+						} />
+						<Route path="/album/:id" element={<AlbumView />} />
+						<Route path="/artist/:id" element={<ArtistView />} />
+
+					</Routes>
+				</Router>
+
+			</div>
+		</DataContext.Provider>
+	);
 }
 
 export default App;
